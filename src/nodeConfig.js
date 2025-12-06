5989cc753173760a085da7f080d7002bf3437417
Kabir Singh <kabirsingh12b@gmail.com>
2025-12-06 15:51:24 +0530

    chore : universal nodeConfig for adding new nodes

---
+++ src/nodeConfig.js
@@ -0,0 +1,10 @@
+export const nodeConfigs = {
+  knowledgeBaseNode: {
+    label: "Knowledge Base",
+    handles: { source: 2, target: 1 },
+  },
+  inputNode: {
+    label: "Input",
+    handles: { source: 1, target: 0 },
+  },
+};
