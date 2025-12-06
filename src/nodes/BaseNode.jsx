60cf41db3ed099095dea3dd13487290220d48850
Kabir Singh <kabirsingh12b@gmail.com>
2025-12-06 18:26:29 +0530

    removed unnecessary lines

--- src/nodes/BaseNode.jsx
+++ src/nodes/BaseNode.jsx
@@ -1,27 +1,14 @@
-import { useState } from "react";
 import { Handle, Position } from "reactflow";
 
 export const NAME = ({ data, id }) => {
   const { label, handles } = data;
-  const [currName, setCurrName] = useState(
-    data?.inputName || id.replace("customInput-", "input_")
-  );
-
-  const handleNameChange = (e) => {
-    setCurrName(e.target.value);
-  };
 
   return (
     <div style={{ width: 200, border: "1px solid" }}>
       <div>
         <span>{label}</span>
       </div>
-      <div>
-        <label>
-          Name:
-          <input type="text" value={currName} onChange={handleNameChange} />
-        </label>
-      </div>
+      <div>This is {label} node</div>
       {Array.from({ length: handles.source }).map((_, index) => {
         return (
           <Handle
