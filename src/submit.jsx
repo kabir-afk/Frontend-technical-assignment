3c8483bbf67997b06080f624856b0c60485c6fb0
Kabir Singh <kabirsingh12b@gmail.com>
2025-12-06 20:58:41 +0530

    feat : sent relevant data using axios

--- src/submit.jsx
+++ src/submit.jsx
@@ -1,5 +1,6 @@
 // submit.js
 import { useStore } from "./store";
+import axios from "axios";
 
 export const SubmitButton = () => {
   const { nodes, edges } = useStore((state) => ({
@@ -7,11 +8,26 @@ export const SubmitButton = () => {
     edges: state.edges,
   }));
 
+  const handleSubmit = async () => {
+    try {
+      const nodesData = nodes.map((e) => e.id);
+      const edgesData = edges.map((e) => {
+        return { id: e.id, source: e.source, target: e.target };
+      });
+      const res = await axios.post("http://localhost:8000/pipelines/parse", {
+        nodesData,
+        edgesData,
+      });
+      console.log(res.data);
+    } catch (error) {
+      console.error("Error : ", error);
+    }
+  };
   return (
     <div className="flex justify-center items-center">
       <button
         type="submit"
-        onClick={() => console.log(nodes, edges)}
+        onClick={handleSubmit}
         className="border cursor-pointer"
       >
         Submit
