c6bf47baa48ee277195a68df4b7c18c2e794693a
Kabir Singh <kabirsingh12b@gmail.com>
2025-12-06 15:50:41 +0530

    feat : nodeGenerator now reads config file for nodes that are not present and creates them along with respective imports

--- nodeGenerator.js
+++ nodeGenerator.js
@@ -1,53 +1,56 @@
 import fs from "fs/promises";
+import { nodeConfigs } from "./src/nodeConfig.js";
 
-async function generate(config) {
+async function generate() {
   try {
-    const { name, type } = config;
+    for (const [key, value] of Object.entries(nodeConfigs)) {
+      const path = `./src/nodes/${key}.jsx`;
+      try {
+        await fs.access(path, fs.constants.F_OK);
+        console.error("Node already exists");
+      } catch (error) {
+        console.log("Creating Node");
+        const name = key;
+        const type = key;
 
-    const template = await fs.readFile("./src/nodes/BaseNode.jsx", "utf8");
-    const output = template
-      .replace("NAME", name)
-      .replace("CONFIG", JSON.stringify(config, null, 2));
-    await fs.writeFile(`./src/nodes/${name}.jsx`, output);
+        const template = await fs.readFile("./src/nodes/BaseNode.jsx", "utf8");
+        const output = template.replace("NAME", name);
+        await fs.writeFile(`./src/nodes/${name}.jsx`, output);
 
-    const toolbar = await fs.readFile("./src/toolbar.jsx", "utf-8");
-    const newNode = `<DraggableNode type="${type}" label="${name}" />\n        <span />`;
-    if (toolbar.includes(newNode)) {
-      throw new Error(`Node '${name}' already exists in toolbar`);
+        const toolbar = await fs.readFile("./src/toolbar.jsx", "utf-8");
+        const newNode = `<DraggableNode type="${type}" label="${value.label}" />\n        <span />`;
+        if (toolbar.includes(newNode)) {
+          throw new Error(`Node '${name}' already exists in toolbar`);
+        }
+        const newToolBar = toolbar.replace("<span />", newNode);
+        await fs.writeFile("./src/toolbar.jsx", newToolBar);
+
+        let ui = await fs.readFile("./src/ui.jsx", "utf-8");
+        const newNodeType = `  ${type}: ${name},\n`;
+        if (ui.includes(newNodeType)) {
+          throw new Error(`Node type '${type}' already exists in ui`);
+        }
+        ui = ui.replace(
+          /(const nodeTypes = \{[^}]*)\n\};/,
+          `$1\n${newNodeType}};`
+        );
+
+        const importStatement = `import { ${name} } from "./nodes/${name}.jsx";`;
+        const nodeImportPattern = /import { \w+ } from "\.\/nodes\/\w+\.jsx";/;
+
+        if (ui.match(nodeImportPattern)) {
+          ui = ui.replace(
+            nodeImportPattern,
+            (match) => match + "\n" + importStatement
+          );
+        }
+
+        await fs.writeFile("./src/ui.jsx", ui);
+      }
     }
-    const newToolBar = toolbar.replace("<span />", newNode);
-    await fs.writeFile("./src/toolbar.jsx", newToolBar);
-
-    let ui = await fs.readFile("./src/ui.jsx", "utf-8");
-    const newNodeType = `  ${type}: ${name},\n`;
-    if (ui.includes(newNodeType)) {
-      throw new Error(`Node type '${type}' already exists in ui`);
-    }
-    ui = ui.replace(/(const nodeTypes = \{[^}]*)\n\};/, `$1\n${newNodeType}};`);
-
-    const importStatement = `import { ${name} } from "./nodes/${name}.jsx";`;
-    const nodeImportPattern = /import { \w+ } from "\.\/nodes\/\w+\.jsx";/;
-
-    if (ui.match(nodeImportPattern)) {
-      ui = ui.replace(
-        nodeImportPattern,
-        (match) => match + "\n" + importStatement
-      );
-    }
-
-    await fs.writeFile("./src/ui.jsx", ui);
   } catch (error) {
     console.error(`Error: ${error.message}`);
   }
 }
 
-const config = {
-  name: "KnowledgeBase4",
-  type: "knowledgeBase4",
-  handles: {
-    source: 2,
-    target: 1,
-  },
-};
-
-generate(config);
+generate();
