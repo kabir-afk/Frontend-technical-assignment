4fba9d1abe7d38c1bc0a6e061074bb1754c1c5b9
Kabir Singh <kabirsingh12b@gmail.com>
2025-12-07 15:25:43 +0530

    update : styles

--- src/nodes/llmNode.jsx
+++ src/nodes/llmNode.jsx
@@ -1,24 +1,39 @@
 // llmNode.js
 
-import { Handle, Position } from 'reactflow';
+import { Handle, Position } from "reactflow";
 
 export const LLMNode = ({ id, data }) => {
-
   return (
-    <div style={{width: 200, height: 80, border: '1px solid black'}}>
+    <div
+      style={{
+        width: 200,
+        boxShadow: "0 0 0 2px #D7D8EC",
+        padding: "4px",
+        borderRadius: "4px",
+        backgroundColor: "white",
+      }}
+    >
       <Handle
         type="target"
         position={Position.Left}
         id={`${id}-system`}
-        style={{top: `${100/3}%`}}
+        style={{
+          top: `${100 / 3}%`,
+          border: "1px solid blue",
+          backgroundColor: "white",
+        }}
       />
       <Handle
         type="target"
         position={Position.Left}
         id={`${id}-prompt`}
-        style={{top: `${200/3}%`}}
+        style={{
+          top: `${200 / 3}%`,
+          border: "1px solid blue",
+          backgroundColor: "white",
+        }}
       />
-      <div>
+      <div className="flex gap-1 bg-purple-light text-purple-dark font-medium rounded-md p-1">
         <span>LLM</span>
       </div>
       <div>
@@ -28,7 +43,11 @@ export const LLMNode = ({ id, data }) => {
         type="source"
         position={Position.Right}
         id={`${id}-response`}
+        style={{
+          border: "1px solid blue",
+          backgroundColor: "white",
+        }}
       />
     </div>
   );
-}
+};
