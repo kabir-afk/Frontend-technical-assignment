7b5586021e097dfd8a3f974c158a24c01d47c7bb
Kabir Singh <kabirsingh12b@gmail.com>
2025-12-07 15:24:03 +0530

    update : styles

--- src/nodes/BaseNode.jsx
+++ src/nodes/BaseNode.jsx
@@ -3,8 +3,16 @@ import { Handle, Position } from "reactflow";
 export const NAME = ({ data, id }) => {
   const { label, handles, Icon } = data;
   return (
-    <div style={{ width: 200, border: "1px solid" }}>
-      <div className="flex gap-1 bg-purple-light text-purple-dark font-medium">
+    <div
+      style={{
+        width: 200,
+        boxShadow: "0 0 0 2px #D7D8EC",
+        padding: "4px",
+        borderRadius: "4px",
+        backgroundColor: "white",
+      }}
+    >
+      <div className="flex gap-1 bg-purple-light text-purple-dark font-medium rounded-md p-1">
         {Icon ? (
           <div
             className="node-icon"
@@ -23,6 +31,8 @@ export const NAME = ({ data, id }) => {
             id={`${id}-value`}
             style={{
               top: `${((index + 1) * 100) / (handles.source + 1)}%`,
+              border: "1px solid blue",
+              backgroundColor: "white",
             }}
           />
         );
@@ -36,6 +46,8 @@ export const NAME = ({ data, id }) => {
             id={`${id}-value`}
             style={{
               top: `${((index + 1) * 100) / (handles.target + 1)}%`,
+              border: "1px solid blue",
+              backgroundColor: "white",
             }}
           />
         );
